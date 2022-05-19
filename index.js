175b14ee443ec2a72cc24e296b0818eef32b3048
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-18 22:12:26 -0700

    commit tree 338bcf6b7f228dfcecd656070ab465b4ebd3b545

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(e){
+  let change = document.getElementById('text')
+  change.innerHTML = "This is really cool!"
+  return change
+  
+})
